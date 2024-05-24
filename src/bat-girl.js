c898e91e83c3cf0ae840bab3f778a75b1ef81c25
Vladimir Sanchez <vladypuggu@gmail.com>
2024-05-24 11:55:46 -0600

    Added title value
    Added paragraph value
    
    Added title1 value
    
    Added  paragraph1 value
    
    Added title2 value
    
    Added paragraph2 value
    
    Added imageURL value
    
    Created title element in template
    
    Created paragraph element in template
    
    Created title1 element in template
    
    Created paragraph1 element in template
    
    Created class "dc-img-container" in template
    
    Created class "personaje-img" in template
    
    Created imageURL element in template

--- src/bat-girl.js
+++ src/bat-girl.js
@@ -4,25 +4,32 @@ export class BatGirl extends LitElement {
   static get properties() {
     return {
 
-      title:{type:String},
-      paragraph:{type:String,},
+      title: { type: String },
+      paragraph: { type: String, },
 
-      title1:{type:String},
-      paragraph1:{type:String},
+      title1: { type: String },
+      paragraph1: { type: String },
 
-      title2:{type:String},
-      paragraph2:{type:String},
+      title2: { type: String },
+      paragraph2: { type: String },
 
-      imageURL:{type:String},
+      imageURL: { type: String },
 
     }
   }
 
   constructor() {
     super()
-    
+    this.title = 'Batgirl'
+    this.paragraph = 'Barbara Gordon, hija del Comisionado de Policía James Gordon, asumió el manto de Batgirl en la década de 1960. Su debut fue en "Detective Comics #359" en 1967. Barbara es una joven brillante, con una maestría en biblioteconomía y ciencias de la información, así como habilidades en artes marciales y un genio intelectual comparable al de su mentor, Batman. '
 
+    this.title1 = 'Origenes'
+    this.paragraph1 = 'Los orígenes de Batgirl varían según la versión. En la historia clásica, Barbara se convierte en Batgirl después de presenciar un crimen y decidir usar su inteligencia y habilidades físicas para luchar contra la injusticia en Gotham City. En la versión más icónica, fue inspirada por Batman y Robin después de salvarlos de un villano llamado Killer Moth en una fiesta. '
 
+    this.title2 = 'Caracteristicas'
+    this.paragraph2 = 'Una de las características más destacadas de Batgirl, especialmente en la representación de Barbara Gordon, es su inteligencia y habilidad para resolver problemas. Además de sus habilidades físicas, es una experta en tecnología y computación. También es conocida por su tenacidad y determinación para enfrentarse a los criminales más peligrosos de Gotham. A lo largo de los años, Batgirl ha evolucionado como personaje, enfrentando desafíos personales y profesionales. En una de las historias más influyentes, "The Killing Joke", Barbara Gordon fue brutalmente herida por el Joker, lo que la dejó paralítica. Después de este evento, adoptó la identidad de Oracle, convirtiéndose en una hacker y estratega experta que proporciona inteligencia y apoyo logístico a otros héroes. Posteriormente, en el cómic "The New 52", DC Comics reintrodujo a Barbara Gordon como Batgirl, utilizando una historia en la que recupera la movilidad de sus piernas gracias a una cirugía experimental. Esta versión de Batgirl es una mezcla de la ingeniosa detective de la Edad de Plata con la tenacidad y determinación de la Barbara Gordon post-"The Killing Joke".  '
+
+    imageURL = 'https://naldzgraphics.net/wp-content/uploads/2012/06/26-batgirl.jpg'
 
   }
 
@@ -30,10 +37,17 @@ export class BatGirl extends LitElement {
     return html`
      <div id="main">
 
+     <h1>${this.title}</h1>
+     <p>${this.paragraph}</p>
 
+     <h1>${this.title1}</h1>
+     <p>${this.paragraph1}</p>
 
-
-    `
+     <div class="dc-img-container">
+      <img class="personaje-img" ${this.imageURL}>
+     </div>
+   
+   `
   }
 
 
@@ -48,14 +62,13 @@ export class BatGirl extends LitElement {
       color:rgb(105, 104, 103)
     }
       
-
-      
+    
     .personaje-img{
       width: 20vw;
   }
   
-  .dc-img-container{
-    text-align:center;
+    .dc-img-container{
+      text-align:center;
   }
 
     `
